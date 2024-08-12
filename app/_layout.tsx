90b244d03376743cd671be371d2641049142e9de
Juan Sebastian <juaaley250@gmail.com>
2024-08-12 15:08:43 -0500

    subiendo el README

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
-import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Slot, Stack } from "expo-router";
+import { Header } from "react-native/Libraries/NewAppScreen";
 
-export default function RootLayout() {
+const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+      <Stack>
+        <Stack.Screen name="index"  />
+      </Stack>
   );
-}
+};
+
+export default RootLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Slot } from "expo-router";
+import React from "react";
 
-export default function Index() {
+const RootLayout = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Slot/>
   );
-}
+};
+
+export default RootLayout;
+
+
